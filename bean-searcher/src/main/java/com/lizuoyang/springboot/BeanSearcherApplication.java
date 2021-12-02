22d92ec10b692680b2ef3fe4bb2d9a655bc609c7
lizuoyang <lizuoyangsina@qq.com>
2021-12-02 11:08:55 +0800

    feat: Bean-Searcher 学习笔记

---
+++ bean-searcher/src/main/java/com/lizuoyang/springboot/BeanSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.lizuoyang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanSearcherApplication.class, args);
+    }
+
+}
