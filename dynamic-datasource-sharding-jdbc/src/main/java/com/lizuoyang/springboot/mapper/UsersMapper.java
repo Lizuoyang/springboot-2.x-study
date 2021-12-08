f8cc9731394fe7fedfc22d9e5b3564f4335a4f44
lizuoyang <lizuoyangsina@qq.com>
2021-12-08 18:06:07 +0800

    feat: sharding-jdbc多数据源配置 学习笔记

---
+++ dynamic-datasource-sharding-jdbc/src/main/java/com/lizuoyang/springboot/mapper/UsersMapper.java
@@ -0,0 +1,16 @@
+package com.lizuoyang.springboot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lizuoyang.springboot.entity.UsersDO;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author mybatis-plus-general
+ * @since 2021-12-07
+ */
+public interface UsersMapper extends BaseMapper<UsersDO> {
+
+}
---
+++ read-and-write-separate/src/main/java/com/lizuoyang/springboot/ReadAndWriteSeparateApplication.java
@@ -0,0 +1,13 @@
+package com.lizuoyang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadAndWriteSeparateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadAndWriteSeparateApplication.class, args);
+    }
+
+}
